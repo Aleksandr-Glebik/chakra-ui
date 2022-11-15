dbaffb689aa52166c86e69891f60e0f0a57b82ec
Aleksandr-Glebik <glebikaa@mail.ru>
2022-11-15 20:22:59 +0300

    feat: add Header

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ import {
   Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
+
 import { Logo } from './Logo';
+import Header from './components/Header';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
+      <Header />
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
+
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
             <Text>
