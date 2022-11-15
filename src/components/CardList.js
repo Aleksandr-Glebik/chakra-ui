0b1a089909b81afa6c4538dabb5124aa241657ed
Aleksandr-Glebik <glebikaa@mail.ru>
2022-11-15 20:57:32 +0300

    feat: setup Card

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,4 +1,4 @@
-import { SimpleGrid, Box, Button, Heading, Text } from '@chakra-ui/react'
+import { SimpleGrid, Stack, Button, Heading, Text } from '@chakra-ui/react'
 
 const CardList = (props) => {
     const {cards = []} = props
@@ -7,17 +7,17 @@ const CardList = (props) => {
         <SimpleGrid columns={[1, null, 2, 3]} spacing={[4, null, 6]} py='4'>
             {
                 cards.map(card => (
-                    <Box key={card.title} >
-                        <Heading as="h5" >
+                    <Stack key={card.title} spacing={2} _hover={{shadow: 'md'}} p="4">
+                        <Heading as="h4" noOfLines={1}>
                             {card.title}
                         </Heading>
                         <Text>
                             {card.body}
                         </Text>
-                        <Button as="a">
+                        <Button as="a" variant="outline">
                             Read more
                         </Button>
-                    </Box>
+                    </Stack>
                 ))
             }
         </SimpleGrid>
